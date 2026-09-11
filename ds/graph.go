5ad212485080db3859f94a224dab26a300abde97
Abir Basak <abirbasak@gmail.com>
2026-09-11 13:11:02 +0530

    feat(ds): add AdjList, IncidenceList and graph interfaces

---
+++ ds/graph.go
@@ -0,0 +1,242 @@
+package ds
+
+import (
+	"iter"
+
+	"github.com/azuiktech/kleisli-go/adt"
+)
+
+type NodeID int
+type EdgeID int
+
+// EdgeListGraph is a graph interface providing iteration over all edges.
+type EdgeListGraph[E any] interface {
+	Edges() iter.Seq2[EdgeID, E]
+}
+
+// NodeListGraph is a graph interface providing iteration over all nodes.
+type NodeListGraph[N any] interface {
+	Nodes() iter.Seq2[NodeID, N]
+}
+
+// IncidenceGraph is a directed graph interface providing outgoing edges and endpoint queries.
+type IncidenceGraph[E any] interface {
+	OutEdges(id NodeID) iter.Seq2[EdgeID, E]
+	Src(id EdgeID) NodeID
+	Dst(id EdgeID) NodeID
+}
+
+// BidirectionalGraph extends IncidenceGraph with incoming edges queries.
+type BidirectionalGraph[E any] interface {
+	IncidenceGraph[E]
+	InEdges(id NodeID) iter.Seq2[EdgeID, E]
+}
+
+type adjNode[N any] struct {
+	outEdges []EdgeID
+	inEdges  []EdgeID
+	data     N
+}
+
+type adjEdge[E any] struct {
+	src  NodeID
+	dst  NodeID
+	data E
+}
+
+type AdjList[N, E any] struct {
+	nodes []adjNode[N]
+	edges []adjEdge[E]
+}
+
+func NewAdjList[N, E any]() *AdjList[N, E] {
+	return &AdjList[N, E]{}
+}
+
+func (a *AdjList[N, E]) AddNode(data N) NodeID {
+	id := NodeID(len(a.nodes))
+	a.nodes = append(a.nodes, adjNode[N]{
+		data: data,
+	})
+	return id
+}
+
+func (a *AdjList[N, E]) AddEdge(data E, src, dst NodeID) EdgeID {
+	id := EdgeID(len(a.edges))
+	a.edges = append(a.edges, adjEdge[E]{
+		src:  src,
+		dst:  dst,
+		data: data,
+	})
+	a.nodes[src].outEdges = append(a.nodes[src].outEdges, id)
+	a.nodes[dst].inEdges = append(a.nodes[dst].inEdges, id)
+	return id
+}
+
+func (a *AdjList[N, E]) Node(id NodeID) adt.Option[N] {
+	if int(id) < 0 || int(id) >= len(a.nodes) {
+		return adt.None[N]()
+	}
+	return adt.Some(a.nodes[id].data)
+}
+
+func (a *AdjList[N, E]) Edge(id EdgeID) adt.Option[E] {
+	if int(id) < 0 || int(id) >= len(a.edges) {
+		return adt.None[E]()
+	}
+	return adt.Some(a.edges[id].data)
+}
+
+func (a *AdjList[N, E]) Nodes() iter.Seq2[NodeID, N] {
+	return func(yield func(NodeID, N) bool) {
+		for i, n := range a.nodes {
+			if !yield(NodeID(i), n.data) {
+				return
+			}
+		}
+	}
+}
+
+func (a *AdjList[N, E]) Edges() iter.Seq2[EdgeID, E] {
+	return func(yield func(EdgeID, E) bool) {
+		for i, e := range a.edges {
+			if !yield(EdgeID(i), e.data) {
+				return
+			}
+		}
+	}
+}
+
+func (a *AdjList[N, E]) OutEdges(id NodeID) iter.Seq2[EdgeID, E] {
+	return func(yield func(EdgeID, E) bool) {
+		if int(id) < 0 || int(id) >= len(a.nodes) {
+			return
+		}
+		for _, eID := range a.nodes[id].outEdges {
+			if !yield(eID, a.edges[eID].data) {
+				return
+			}
+		}
+	}
+}
+
+func (a *AdjList[N, E]) InEdges(id NodeID) iter.Seq2[EdgeID, E] {
+	return func(yield func(EdgeID, E) bool) {
+		if int(id) < 0 || int(id) >= len(a.nodes) {
+			return
+		}
+		for _, eID := range a.nodes[id].inEdges {
+			if !yield(eID, a.edges[eID].data) {
+				return
+			}
+		}
+	}
+}
+
+func (a *AdjList[N, E]) Src(id EdgeID) NodeID {
+	return a.edges[id].src
+}
+
+func (a *AdjList[N, E]) Dst(id EdgeID) NodeID {
+	return a.edges[id].dst
+}
+
+type incNode[N any] struct {
+	outEdges []EdgeID
+	data     N
+}
+
+type IncidenceList[N, E any] struct {
+	nodes []incNode[N]
+	edges []adjEdge[E]
+}
+
+func NewIncidenceList[N, E any]() *IncidenceList[N, E] {
+	return &IncidenceList[N, E]{}
+}
+
+func (a *IncidenceList[N, E]) AddNode(data N) NodeID {
+	id := NodeID(len(a.nodes))
+	a.nodes = append(a.nodes, incNode[N]{
+		data: data,
+	})
+	return id
+}
+
+func (a *IncidenceList[N, E]) AddEdge(data E, src, dst NodeID) EdgeID {
+	id := EdgeID(len(a.edges))
+	a.edges = append(a.edges, adjEdge[E]{
+		src:  src,
+		dst:  dst,
+		data: data,
+	})
+	a.nodes[src].outEdges = append(a.nodes[src].outEdges, id)
+	return id
+}
+
+func (a *IncidenceList[N, E]) Node(id NodeID) adt.Option[N] {
+	if int(id) < 0 || int(id) >= len(a.nodes) {
+		return adt.None[N]()
+	}
+	return adt.Some(a.nodes[id].data)
+}
+
+func (a *IncidenceList[N, E]) Edge(id EdgeID) adt.Option[E] {
+	if int(id) < 0 || int(id) >= len(a.edges) {
+		return adt.None[E]()
+	}
+	return adt.Some(a.edges[id].data)
+}
+
+func (a *IncidenceList[N, E]) Nodes() iter.Seq2[NodeID, N] {
+	return func(yield func(NodeID, N) bool) {
+		for i, n := range a.nodes {
+			if !yield(NodeID(i), n.data) {
+				return
+			}
+		}
+	}
+}
+
+func (a *IncidenceList[N, E]) Edges() iter.Seq2[EdgeID, E] {
+	return func(yield func(EdgeID, E) bool) {
+		for i, e := range a.edges {
+			if !yield(EdgeID(i), e.data) {
+				return
+			}
+		}
+	}
+}
+
+func (a *IncidenceList[N, E]) OutEdges(id NodeID) iter.Seq2[EdgeID, E] {
+	return func(yield func(EdgeID, E) bool) {
+		if int(id) < 0 || int(id) >= len(a.nodes) {
+			return
+		}
+		for _, eID := range a.nodes[id].outEdges {
+			if !yield(eID, a.edges[eID].data) {
+				return
+			}
+		}
+	}
+}
+
+func (a *IncidenceList[N, E]) Src(id EdgeID) NodeID {
+	return a.edges[id].src
+}
+
+func (a *IncidenceList[N, E]) Dst(id EdgeID) NodeID {
+	return a.edges[id].dst
+}
+
+// Compile-time interface satisfaction checks.
+var (
+	_ EdgeListGraph[int]      = (*AdjList[string, int])(nil)
+	_ NodeListGraph[string]   = (*AdjList[string, int])(nil)
+	_ IncidenceGraph[int]     = (*AdjList[string, int])(nil)
+	_ BidirectionalGraph[int] = (*AdjList[string, int])(nil)
+
+	_ EdgeListGraph[int]    = (*IncidenceList[string, int])(nil)
+	_ NodeListGraph[string] = (*IncidenceList[string, int])(nil)
+	_ IncidenceGraph[int]   = (*IncidenceList[string, int])(nil)
+)
---
+++ ds/graph_test.go
@@ -0,0 +1,343 @@
+package ds_test
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/azuiktech/kleisli-go/ds"
+)
+
+func TestAdjList_Empty(t *testing.T) {
+	g := ds.NewAdjList[string, int]()
+
+	// Nodes iteration on empty graph
+	count := 0
+	for range g.Nodes() {
+		count++
+	}
+	if count != 0 {
+		t.Fatalf("expected 0 nodes, got %d", count)
+	}
+
+	// Edges iteration on empty graph
+	edgeCount := 0
+	for range g.Edges() {
+		edgeCount++
+	}
+	if edgeCount != 0 {
+		t.Fatalf("expected 0 edges, got %d", edgeCount)
+	}
+
+	// OutEdges and InEdges for non-existent node
+	outCount := 0
+	for range g.OutEdges(ds.NodeID(0)) {
+		outCount++
+	}
+	if outCount != 0 {
+		t.Fatalf("expected 0 out edges, got %d", outCount)
+	}
+
+	inCount := 0
+	for range g.InEdges(ds.NodeID(0)) {
+		inCount++
+	}
+	if inCount != 0 {
+		t.Fatalf("expected 0 in edges, got %d", inCount)
+	}
+
+	// Direct lookups
+	if g.Node(ds.NodeID(0)).IsSome() {
+		t.Fatal("expected Node(0) to be None")
+	}
+	if g.Edge(ds.EdgeID(0)).IsSome() {
+		t.Fatal("expected Edge(0) to be None")
+	}
+}
+
+func TestAdjList_AddNodeAndLookup(t *testing.T) {
+	g := ds.NewAdjList[string, int]()
+
+	n0 := g.AddNode("alpha")
+	n1 := g.AddNode("beta")
+	n2 := g.AddNode("gamma")
+
+	if n0 != 0 || n1 != 1 || n2 != 2 {
+		t.Fatalf("unexpected node IDs: %d, %d, %d", n0, n1, n2)
+	}
+
+	// Direct lookups
+	optN0 := g.Node(n0)
+	if optN0.IsNone() || optN0.MustGet() != "alpha" {
+		t.Fatalf("unexpected node 0: %v", optN0)
+	}
+	optN1 := g.Node(n1)
+	if optN1.IsNone() || optN1.MustGet() != "beta" {
+		t.Fatalf("unexpected node 1: %v", optN1)
+	}
+	optN2 := g.Node(n2)
+	if optN2.IsNone() || optN2.MustGet() != "gamma" {
+		t.Fatalf("unexpected node 2: %v", optN2)
+	}
+
+	// Out of bounds lookups
+	if g.Node(ds.NodeID(-1)).IsSome() {
+		t.Fatal("expected None for negative node ID")
+	}
+	if g.Node(ds.NodeID(3)).IsSome() {
+		t.Fatal("expected None for out of range node ID")
+	}
+
+	// Nodes() iterator
+	var collectedIDs []ds.NodeID
+	var collectedData []string
+	for id, data := range g.Nodes() {
+		collectedIDs = append(collectedIDs, id)
+		collectedData = append(collectedData, data)
+	}
+
+	if !slices.Equal(collectedIDs, []ds.NodeID{0, 1, 2}) {
+		t.Fatalf("unexpected node IDs: %v", collectedIDs)
+	}
+	if !slices.Equal(collectedData, []string{"alpha", "beta", "gamma"}) {
+		t.Fatalf("unexpected node data: %v", collectedData)
+	}
+}
+
+func TestAdjList_AddEdgeAndTraversal(t *testing.T) {
+	// Build graph:
+	// A(0) -> B(1) [edge 0: 100]
+	// B(1) -> C(2) [edge 1: 200]
+	// A(0) -> C(2) [edge 2: 300]
+	g := ds.NewAdjList[string, int]()
+
+	nA := g.AddNode("A")
+	nB := g.AddNode("B")
+	nC := g.AddNode("C")
+
+	e0 := g.AddEdge(100, nA, nB)
+	e1 := g.AddEdge(200, nB, nC)
+	e2 := g.AddEdge(300, nA, nC)
+
+	if e0 != 0 || e1 != 1 || e2 != 2 {
+		t.Fatalf("unexpected edge IDs: %d, %d, %d", e0, e1, e2)
+	}
+
+	// Direct edge lookups
+	if g.Edge(e0).MustGet() != 100 {
+		t.Fatalf("expected edge 0 to have 100, got %v", g.Edge(e0))
+	}
+	if g.Edge(e1).MustGet() != 200 {
+		t.Fatalf("expected edge 1 to have 200, got %v", g.Edge(e1))
+	}
+	if g.Edge(e2).MustGet() != 300 {
+		t.Fatalf("expected edge 2 to have 300, got %v", g.Edge(e2))
+	}
+	if g.Edge(ds.EdgeID(-1)).IsSome() || g.Edge(ds.EdgeID(99)).IsSome() {
+		t.Fatal("expected None for out of bounds edge IDs")
+	}
+
+	// Src and Dst
+	if g.Src(e0) != nA || g.Dst(e0) != nB {
+		t.Fatalf("unexpected endpoints for e0: src=%d, dst=%d", g.Src(e0), g.Dst(e0))
+	}
+	if g.Src(e1) != nB || g.Dst(e1) != nC {
+		t.Fatalf("unexpected endpoints for e1: src=%d, dst=%d", g.Src(e1), g.Dst(e1))
+	}
+	if g.Src(e2) != nA || g.Dst(e2) != nC {
+		t.Fatalf("unexpected endpoints for e2: src=%d, dst=%d", g.Src(e2), g.Dst(e2))
+	}
+
+	// OutEdges of A
+	var outAIDs []ds.EdgeID
+	var outAData []int
+	for eid, edata := range g.OutEdges(nA) {
+		outAIDs = append(outAIDs, eid)
+		outAData = append(outAData, edata)
+	}
+	if !slices.Equal(outAIDs, []ds.EdgeID{e0, e2}) || !slices.Equal(outAData, []int{100, 300}) {
+		t.Fatalf("unexpected OutEdges(A): ids=%v, data=%v", outAIDs, outAData)
+	}
+
+	// InEdges of C
+	var inCIDs []ds.EdgeID
+	var inCData []int
+	for eid, edata := range g.InEdges(nC) {
+		inCIDs = append(inCIDs, eid)
+		inCData = append(inCData, edata)
+	}
+	if !slices.Equal(inCIDs, []ds.EdgeID{e1, e2}) || !slices.Equal(inCData, []int{200, 300}) {
+		t.Fatalf("unexpected InEdges(C): ids=%v, data=%v", inCIDs, inCData)
+	}
+
+	// InEdges of A should be empty
+	inACount := 0
+	for range g.InEdges(nA) {
+		inACount++
+	}
+	if inACount != 0 {
+		t.Fatalf("expected 0 InEdges for A, got %d", inACount)
+	}
+
+	// All Edges iterator
+	var allEdges []ds.EdgeID
+	for eid := range g.Edges() {
+		allEdges = append(allEdges, eid)
+	}
+	if !slices.Equal(allEdges, []ds.EdgeID{e0, e1, e2}) {
+		t.Fatalf("unexpected Edges(): %v", allEdges)
+	}
+}
+
+func TestIncidenceList_Basics(t *testing.T) {
+	g := ds.NewIncidenceList[string, float64]()
+
+	n0 := g.AddNode("first")
+	n1 := g.AddNode("second")
+
+	e0 := g.AddEdge(3.14, n0, n1)
+
+	// Direct lookups
+	if g.Node(n0).MustGet() != "first" || g.Node(n1).MustGet() != "second" {
+		t.Fatal("unexpected node data in IncidenceList")
+	}
+	if g.Edge(e0).MustGet() != 3.14 {
+		t.Fatal("unexpected edge data in IncidenceList")
+	}
+
+	// Endpoints
+	if g.Src(e0) != n0 || g.Dst(e0) != n1 {
+		t.Fatalf("mismatched endpoints in IncidenceList: src=%d, dst=%d", g.Src(e0), g.Dst(e0))
+	}
+
+	// OutEdges of n0
+	var out0 []ds.EdgeID
+	for eid := range g.OutEdges(n0) {
+		out0 = append(out0, eid)
+	}
+	if !slices.Equal(out0, []ds.EdgeID{e0}) {
+		t.Fatalf("unexpected OutEdges(n0): %v", out0)
+	}
+
+	// OutEdges of n1 should be empty
+	out1Count := 0
+	for range g.OutEdges(n1) {
+		out1Count++
+	}
+	if out1Count != 0 {
+		t.Fatalf("expected 0 OutEdges for n1, got %d", out1Count)
+	}
+}
+
+func TestGraph_InterfacePolymorphism(t *testing.T) {
+	adj := ds.NewAdjList[string, string]()
+	nA := adj.AddNode("NodeA")
+	nB := adj.AddNode("NodeB")
+	adj.AddEdge("A->B", nA, nB)
+
+	inc := ds.NewIncidenceList[string, string]()
+	n0 := inc.AddNode("Node0")
+	n1 := inc.AddNode("Node1")
+	inc.AddEdge("0->1", n0, n1)
+
+	// Helper functions testing interface contracts
+	countNodes := func(g ds.NodeListGraph[string]) int {
+		c := 0
+		for range g.Nodes() {
+			c++
+		}
+		return c
+	}
+
+	countEdges := func(g ds.EdgeListGraph[string]) int {
+		c := 0
+		for range g.Edges() {
+			c++
+		}
+		return c
+	}
+
+	findOutgoingTargets := func(g ds.IncidenceGraph[string], start ds.NodeID) []ds.NodeID {
+		var targets []ds.NodeID
+		for eid := range g.OutEdges(start) {
+			targets = append(targets, g.Dst(eid))
+		}
+		return targets
+	}
+
+	findIncomingSources := func(g ds.BidirectionalGraph[string], target ds.NodeID) []ds.NodeID {
+		var sources []ds.NodeID
+		for eid := range g.InEdges(target) {
+			sources = append(sources, g.Src(eid))
+		}
+		return sources
+	}
+
+	// Both satisfy NodeListGraph
+	if countNodes(adj) != 2 || countNodes(inc) != 2 {
+		t.Fatal("NodeListGraph polymorphism failed")
+	}
+
+	// Both satisfy EdgeListGraph
+	if countEdges(adj) != 1 || countEdges(inc) != 1 {
+		t.Fatal("EdgeListGraph polymorphism failed")
+	}
+
+	// Both satisfy IncidenceGraph
+	if !slices.Equal(findOutgoingTargets(adj, nA), []ds.NodeID{nB}) {
+		t.Fatal("IncidenceGraph OutEdges on AdjList failed")
+	}
+	if !slices.Equal(findOutgoingTargets(inc, n0), []ds.NodeID{n1}) {
+		t.Fatal("IncidenceGraph OutEdges on IncidenceList failed")
+	}
+
+	// Only AdjList satisfies BidirectionalGraph
+	if !slices.Equal(findIncomingSources(adj, nB), []ds.NodeID{nA}) {
+		t.Fatal("BidirectionalGraph InEdges on AdjList failed")
+	}
+}
+
+func TestGraph_EarlyBreakIterator(t *testing.T) {
+	g := ds.NewAdjList[int, int]()
+	for i := 0; i < 10; i++ {
+		g.AddNode(i)
+	}
+	for i := 1; i < 10; i++ {
+		g.AddEdge(i*10, ds.NodeID(0), ds.NodeID(i))
+	}
+
+	// Break early in Nodes()
+	nodesCount := 0
+	for id := range g.Nodes() {
+		nodesCount++
+		if id == 3 {
+			break
+		}
+	}
+	if nodesCount != 4 {
+		t.Fatalf("expected early break after 4 nodes, got %d", nodesCount)
+	}
+
+	// Break early in Edges()
+	edgesCount := 0
+	for eid := range g.Edges() {
+		edgesCount++
+		if eid == 2 {
+			break
+		}
+	}
+	if edgesCount != 3 {
+		t.Fatalf("expected early break after 3 edges, got %d", edgesCount)
+	}
+
+	// Break early in OutEdges()
+	outCount := 0
+	for eid := range g.OutEdges(ds.NodeID(0)) {
+		outCount++
+		if eid == 1 {
+			break
+		}
+	}
+	if outCount != 2 {
+		t.Fatalf("expected early break after 2 out edges, got %d", outCount)
+	}
+}
